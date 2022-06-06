19fd0eefaf30acae8fce5c05b86fa45e11df0542
mailenalsina <mailen.alsina@davinci.edu.ar>
2022-06-06 20:26:11 -0300

    Cree el componente ItemCount

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import './App.css';
 import BsNavBar from './components/BsNavBar';
-import ItemListContainer from './components/ItemListContainer';
+import ItemListContainer from './containers/ItemListContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemCount from './components/ItemCount';
 
 
 
 function App() {
+
+  const onAdd = (count) => {
+
+    alert(count);
+    
+  }
 
     return (      
       
       <>
            <BsNavBar />
            <ItemListContainer greeting = {'Hola'}/>
+           <ItemCount inicial={1} max={10} onAdd={onAdd}/>
       </>
 
     );
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+const ItemCount = ({inicial, max, onAdd}) => {
+
+    const[count, setCount] = useState()
+
+    const sumar = () => {
+
+        if (count < max) {
+
+            setCount(count + 1)
+
+        } else {
+
+            alert("No podés agregar más productos")
+
+        }
+    }
+
+    const restar = () => {
+
+        if (count < inicial) {
+
+            setCount(count - 1)
+
+        } else {
+
+            alert("No podés quitar más productos")
+
+        }
+        
+    }
+
+        return (
+            <div>
+                <h2>{count}</h2>
+                <button onClick={sumar}>+</button>
+                <button onClick={restar}>-</button>
+                <button onClick={() => onAdd(count)}>Agregar al carrito</button>
+            </div>
+        )
+}
+
+export default ItemCount;
