77ad6744653d08445c7b740913be1ce82743b4b9
mailenalsina <mailen.alsina@davinci.edu.ar>
2022-06-13 20:24:21 -0300

    modifique error e ItemList

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Item from './Item'
 
-export const ItemList = (users) => {
+export const ItemList = ({users}) => {
     return (
         <div>
             {users.map(user => <Item user={user}/>)}
--- src/containers/ItemListContainer.jsx
+++ src/containers/ItemListContainer.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import ItemList from '../components/ItemList'
 
-function ItemListContainer({ }) {
+function ItemListContainer({}) {
     const[ users, setUsers ] = useState([]);
 
     useEffect(() => {
